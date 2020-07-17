4d1313481d3c134cdf5a8649327b95ab33c937a3
Johnny Vu <vujt@hotmail.com>
2020-07-17 11:15:27 -0700

    Add talk controller

---
+++ Controllers/TalksController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/camps/{moniker}/talks")]
+    [ApiController]
+    public class TalksController : ControllerBase
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
+
+        public TalksController(ICampRepository repository, IMapper mapper, LinkGenerator linkGenerator)
+        {
+            this._repository = repository;
+            this._mapper = mapper;
+            this._linkGenerator = linkGenerator;
+        }
+
+        // GET: api/<TalksController>
+        [HttpGet]
+        public async Task<ActionResult<TalkModel[]>> Get(string moniker)
+        {
+            try
+            {
+                var talks = await _repository.GetTalksByMonikerAsync(moniker, true);
+
+                if (talks == null) return NotFound();
+
+                return _mapper.Map<TalkModel[]>(talks);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            };
+        }
+
+        // GET api/<TalksController>/5
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<TalkModel>> Get(string moniker, int id)
+        {
+            try
+            {
+                var talk = await _repository.GetTalkByMonikerAsync(moniker, id, true);
+
+                if (talk == null) return NotFound($"Talk id {id} not found");
+
+                return _mapper.Map<TalkModel>(talk);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            };
+        }
+
+        // POST api/<TalksController>
+        [HttpPost]
+        public async Task<ActionResult<TalkModel>> Post(string moniker, TalkModel model)
+        {
+            try
+            {
+                var camp = await _repository.GetCampAsync(moniker);
+                if (camp == null)
+                {
+                    return BadRequest("Camp does not exist");
+                }
+
+                // create a new talk
+                var talk = _mapper.Map<Talk>(model);
+                talk.Camp = camp;
+
+                if (model.Speaker == null)
+                {
+                    return BadRequest("Speaker ID is required");
+                }
+
+                var speaker = await _repository.GetSpeakerAsync(model.Speaker.SpeakerId);
+                if (speaker == null)
+                {
+                    return BadRequest("Speaker could not be found");
+                }
+
+                // link speaker to talk
+                talk.Speaker = speaker;
+
+                _repository.Add(talk);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    var location = _linkGenerator.GetPathByAction(
+                        HttpContext,
+                        "Get",
+                        values: new { moniker, id = talk.TalkId }
+                        );
+
+                    return Created(location, _mapper.Map<TalkModel>(talk));
+                }
+                else
+                {
+                    return BadRequest("Faile to save new Talk");
+                }
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+            return BadRequest();
+        }
+
+        // PUT api/<TalksController>/5
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<TalkModel>> Put(string moniker, int id, TalkModel model)
+        {
+            try
+            {
+                var talk = await _repository.GetTalkByMonikerAsync(moniker, id, true);
+
+                if (talk == null) return NotFound("Could not find the talk");
+
+                _mapper.Map(model, talk);
+
+                if (model.Speaker != null)
+                {
+                    var speaker = await _repository.GetSpeakerAsync(model.Speaker.SpeakerId);
+
+                    if (speaker != null)
+                    {
+                        talk.Speaker = speaker;
+                    }
+                }
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return _mapper.Map<TalkModel>(talk);
+                }
+                else
+                {
+                    return BadRequest("Faile to update Talk");
+                }
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            };
+        }
+
+        // DELETE api/<TalksController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string moniker, int id)
+        {
+            try
+            {
+                var talk = await _repository.GetTalkByMonikerAsync(moniker, id);
+                if (talk == null)
+                {
+                    return NotFound($"Could not find talk id {id} to delete");
+                }
+
+                _repository.Delete(talk);
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok();
+                }
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+            return BadRequest("Failed to delete the talk");
+        }
+    }
+}
--- Data/CampProfile.cs
+++ Data/CampProfile.cs
@@ -17,7 +17,11 @@ namespace CoreCodeCamp.Data
                 .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName))
                 .ReverseMap();
 
-            this.CreateMap<Talk, TalkModel>().ReverseMap();
+            this.CreateMap<Talk, TalkModel>()
+                .ReverseMap()
+                .ForMember(t => t.Camp, opt => opt.Ignore())
+                .ForMember(t => t.Speaker, opt => opt.Ignore());
+
             this.CreateMap<Speaker, SpeakerModel>().ReverseMap();
         }
     }
